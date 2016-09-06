70c560542dfe3c247de17089b292ebb98c4534cc
nialfe <nialfe@umich.edu>
2016-09-05 20:43:08 -0500

    add gitignore

--- wikipediaHomepageScraping.py
+++ wikipediaHomepageScraping.py
@@ -1,5 +1,5 @@
 #Imports move these to __init__.py
-import urllib
+import csv, os, urllib
 from bs4 import BeautifulSoup
 
 class WikipediaHomepageScraping(object):
@@ -7,24 +7,34 @@ class WikipediaHomepageScraping(object):
     self.wikiLinks = []
     self.numWikiLinks = 0
     self.db = {}
+    self.baseUrl = "http://www.wikipedia.org/"
+    self.linksName = "wikiLinks.txt"
+    self.csvName = "NonProfits.csv"
 
     self.getWikiLinks(categoryPageUrl)
-
-    self.baseUrl = "http://www.wikipedia.org/"
     self.getAllHomepages()
 
   def getWikiLinks(self, categoryPageUrl):
-    links = self.getSoup(categoryPageUrl).find("div", {"class" : "mw-category"})
+    if not os.path.isfile(self.linksName) or os.path.getsize(self.linksName) == 0:
+      links = self.getSoup(categoryPageUrl).find("div", {"class" : "mw-category"})
 
-    for link in links.find_all("a"):
-      orgName, href = self.extractHrefAndText(link, True)
-      self.wikiLinks.append(href)
+      for link in links.find_all("a"):
+        href = self.extractHref(link)
+        self.wikiLinks.append(href)
+        self.numWikiLinks += 1
+      self.writeLinksToFile(self.wikiLinks)
+    else:
+      self.loadFromUrls(self.linksName)
+      print "Using old wikiLinks file"
 
   def getAllHomepages(self):
-    for link in self.wikiLinks:
-      link = self.baseUrl + link
-      self.getHomepageUrl(link)
-    self.printHomepages()
+    if not os.path.isfile(self.csvName) or os.path.getsize(self.csvName) == 0:
+      for link in self.wikiLinks:
+        link = self.baseUrl + link
+        self.getHomepageUrl(link)
+      self.writeToCsv()
+    else:
+      self.readCsvData(self.csvName)
 
   def getHomepageUrl(self, wikiLink):
     print "getting %s", wikiLink
@@ -35,19 +45,19 @@ class WikipediaHomepageScraping(object):
       return
     unorderedlistElt = child.parent.next_sibling.next_sibling
     link = unorderedlistElt.find('a')
-    orgName, href = self.extractHrefAndText(link, False)
+    href = self.extractHref(link)
+    orgName = self.extractOrgName(soup)
     self.db[orgName] = href
 
 ### HELPER FUNCTIONS
-    #Input: an anchor tag and a boolean value whether it should increment the    instance var numWikiLinks
-    #Effects: returns innerHtml and href
-    #  Will increment if needed
-  def extractHrefAndText(self, link, increment):
+  def extractHref(self, link):
     href = link.get('href')
-    orgName = link.getText()
-    if increment:
-      self.numWikiLinks +=1
-    return orgName, href
+    return href
+
+  def extractOrgName(self, soup):
+    child = soup.find(id="firstHeading")
+    return child.getText()
+
 
   def getSoup(self, url):
     r = urllib.urlopen(url).read()
@@ -60,6 +70,37 @@ class WikipediaHomepageScraping(object):
       count += 1
       print key, ' ', value
     print "Got %d homepages" % count
+
+  def writeLinksToFile(self, links):
+    f = open(self.linksName, "w")
+    for link in links:
+      link = link + '\n'
+      f.write(link)
+    f.close()
+
+  def loadFromUrls(self, fileName):
+    f = open(fileName, 'r')
+    self.wikiLinks = f.readlines()
+    f.close()
+
+  def readCsvData(self, fileName):
+    f = open(fileName, 'rb')
+    try:
+      reader = csv.reader(f, delimiter=',')
+      for row in reader:
+      	self.db[row[0]] = row[1]
+    finally:
+      f.close()
+
+  def writeToCsv(self):
+    f = open(self.csvName, 'w')
+    try:
+      writer = csv.writer(f)
+      writer.writerow( ('Name', 'Website', 'Address', 'Number', 'Hours', 'Subject') )
+      for key, value in self.db.iteritems():
+        writer.writerow([key, value])
+    finally:
+      f.close()
 """
   def scrapeHomepage(self, url):
 
